6e416b4d26866f61dc511945525647796e3622dd
bookpark <bkbkgg@gmail.com>
2022-11-11 11:40:01 +0900

    기본 Component 예제

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyCom from './MyCom';
+
+function App() {
+  return (
+    <div>
+      <h1>Start React!</h1>
+      <p>HTML 적용하기</p>
+      <MyCom />
+    </div>
+  );
+}
+export default App;
