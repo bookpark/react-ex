39b0cf627a431e81f6a220f6744929580eee5d95
bookpark <bkbkgg@gmail.com>
2022-11-17 17:14:07 +0900

    SweetAlert2 예제

--- strapapp/src/SweetAlert2Basic.js
+++ strapapp/src/SweetAlert2Basic.js
@@ -6,10 +6,10 @@ class SweetAlert2Basic extends Component {
     componentDidMount() {
         Swal.fire('1. SweetAlert').then(result => { alert('2. result.value: ' + result.value) })
     }
-    
+
     render() {
         return (
-            <h1>sweetalert2 test</h1>
+            <h1>SweetAlert2 Test</h1>
         )
     }
 }
