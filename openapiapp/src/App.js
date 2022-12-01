bd4283e7791e1ea309f1855b7b6fa0cf9efcb379
bookpark <bkbkgg@gmail.com>
2022-12-01 16:11:58 +0900

    다음 주소 api 활용

--- openapiapp/src/App.js
+++ openapiapp/src/App.js
@@ -1,10 +1,12 @@
 import SubwayAPI from "./SubwayAPI";
 import "mvp.css";
 import "./App.css";
+import Postcode from "./Postcode";
 
 function App() {
   return (
     <div className="App">
+      <Postcode />
       <SubwayAPI />
       {/* <RechartsLine /> */}
     </div>
