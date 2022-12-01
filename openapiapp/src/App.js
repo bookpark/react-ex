56c0283f7e87c259ec946d3bd5c180cde86de150
bookpark <bkbkgg@gmail.com>
2022-12-01 11:37:49 +0900

    Recharts 예제 적용

--- openapiapp/src/App.js
+++ openapiapp/src/App.js
@@ -1,10 +1,12 @@
 import SubwayAPI from "./SubwayAPI";
 import "mvp.css";
+import RechartsLine from "./RechartsLine";
 
 function App() {
   return (
     <div className="App">
       <SubwayAPI />
+      <RechartsLine />
     </div>
   );
 }
