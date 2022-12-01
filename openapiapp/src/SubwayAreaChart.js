40ab7ac8959ef9802a50d9d61db1a6b7f0f784a7
bookpark <bkbkgg@gmail.com>
2022-12-01 15:04:43 +0900

    AreaChart 추가

---
+++ openapiapp/src/SubwayAreaChart.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import {
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  AreaChart,
+  Area,
+} from "recharts";
+
+function SubwayAreaChart({ row }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(
+      row.map((item) => ({
+        역이름: item.SUB_STA_NM,
+        유동인구수: item.RIDE_PASGR_NUM + item.ALIGHT_PASGR_NUM,
+      }))
+    );
+  }, []);
+
+  return (
+    <AreaChart
+      width={1000}
+      height={300}
+      data={data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid strokeDasharray={"3 3"} />
+      <XAxis dataKey={"역이름"} />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Area
+        type="monotone"
+        dataKey={"유동인구수"}
+        stroke="#8884D8"
+        activeDot={{ r: 8 }}
+      />
+    </AreaChart>
+  );
+}
+
+export default SubwayAreaChart;
