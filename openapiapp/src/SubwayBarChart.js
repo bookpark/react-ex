c3a325a1245f92ade2d465868ba57538963559d4
bookpark <bkbkgg@gmail.com>
2022-12-01 15:04:54 +0900

    BarChart 추가

---
+++ openapiapp/src/SubwayBarChart.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import {
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  BarChart,
+  Bar,
+} from "recharts";
+
+function SubwayBarChart({ row }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(
+      row.map((item) => ({
+        역이름: item.SUB_STA_NM,
+        유동인구수: item.RIDE_PASGR_NUM + item.ALIGHT_PASGR_NUM,
+      }))
+    );
+  }, []);
+
+  return (
+    <BarChart
+      width={1000}
+      height={300}
+      data={data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid strokeDasharray={"3 3"} />
+      <XAxis dataKey={"역이름"} />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar
+        type="monotone"
+        dataKey={"유동인구수"}
+        fill="#8884D8"
+      />
+    </BarChart>
+  );
+}
+
+export default SubwayBarChart;
